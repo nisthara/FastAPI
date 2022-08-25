8ed317935e7d7e5e55791f4e2827c7c4df79419a
Nisthara <Nisthara@INDD-8G4YXC2.estuate.com>
2022-08-25 09:36:28 +0530

    adding CRUD operations

--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
+#####basic working of fast api's
 #from typing import Union
 
 #from fastapi import FastAPI
@@ -9,11 +10,12 @@
 #def read_root():
  #   return {"Hello": "World"}
 
-
+#path parameter 
 #@app.get("/items/{item_id}")
 #def read_item(item_id: int):
  #   return {"item_id": item_id}
     
+ ###ordering of path parameter
 #@app.get("/user/me")
 #async def read_userme():
  #   return {"user_id":"current user"}
@@ -25,8 +27,36 @@
  
  
  
- #enumulators
-from enum import Enum
+ ####enumulators
+#from enum import Enum
+
+#from fastapi import FastAPI
+
+
+# class ModelName(str, Enum):
+    # alexnet = "alexnet"
+    # resnet = "resnet"
+    # lenet = "lenet"
+
+
+# app = FastAPI()
+
+
+# @app.get("/models/{model_name}")
+# async def get_model(model_name: ModelName):
+    # if model_name == ModelName.alexnet:
+        # return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    # if model_name.value == "lenet":
+        # return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    # return {"model_name": model_name, "message": "Have some residuals"}
+    
+    
+    
+    
+    
+ from enum import Enum
 
 from fastapi import FastAPI
 
