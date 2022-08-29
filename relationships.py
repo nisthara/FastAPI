46ed7200f23a1f5f52abc68b1688f9aa4fe314e8
Nisthara <Nisthara@INDD-8G4YXC2.estuate.com>
2022-08-29 11:59:09 +0530

    commiting the updates

--- relationships.py
+++ relationships.py
@@ -1,27 +1,11 @@
-# from sqlalchemy import Column, ForeignKey, Integer, Table
-# from sqlalchemy.orm import declarative_base, relationship
-
-# Base = declarative_base()
-
-# class Parent(Base):
-    # __tablename__ = "parent"
-    # id = Column(Integer, primary_key=True)
-    # children = relationship("Child")
-
-
-# class Child(Base):
-    # __tablename__ = "child"
-    # id = Column(Integer, primary_key=True)
-    # parent_id = Column(Integer, ForeignKey("parent.id"))
-    
-
-
 import urllib
 from sqlalchemy import create_engine, ForeignKey, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
 from sqlalchemy.orm import relationship
+from fastapi import FastAPI
 
+app = FastAPI()
+Base = declarative_base()
 params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
                                  "SERVER=localhost;"
                                  "DATABASE=estdb;"
@@ -46,5 +30,14 @@ class Invoice(Base):
    amount = Column(Integer)
    customer = relationship("Customer", back_populates = "invoices")
 
-Customer.invoices = relationship("Invoice", order_by = Invoice.id, back_populates = "customer")
-Base.metadata.create_all(engine)
+@app.get("/connect")
+async def read():
+   return{"message":"connected"}
+
+@app.get("/create")
+async def create():
+   Customer.invoices = relationship("Invoice", order_by = Invoice.id, back_populates = "customer")
+   Base.metadata.create_all(engine)
+   return {"message":"relatonship created"}
+
+
