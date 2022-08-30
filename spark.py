3646d41c1ce791b41f77ef8703633c7d08b736fe
Nisthara <Nisthara@INDD-8G4YXC2.estuate.com>
2022-08-30 12:51:23 +0530

    adding

--- spark.py
+++ spark.py
@@ -3,25 +3,18 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pyspark.sql import SparkSession
-
-# engine=None
-# server = 'localhost,1433' # to specify an alternate port
-# database = 'estdb' 
-# username = 'sa' 
-# password = 'estuate@123'
-# params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
-#                                  "SERVER=localhost;"
-#                                  "DATABASE=estdb;"
-#                                  "UID=sa;"
-#                                  "PWD=estuate@123")
-# print(params)                                 
-# engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
-# print("mssql+pyodbc:///?odbc_connect={}".format(params))
-# connection=engine.connect()
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base = declarative_base()
+import csv
+import pandas as pd
 
 spark = SparkSession \
     .builder \
     .appName("Spark session") \
     .getOrCreate()
+
+df = pd.read_csv('exported_csv.csv')
+df.to_parquet('output.parquet')
+# file = open(exported_csv.csv)
+# type(file)
+# csvreader = csv.reader(file)
+
+# file.close()
